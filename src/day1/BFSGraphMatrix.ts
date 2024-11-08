66bcc58e47e1cdc93a744bfaeece4016781eca88
eduardo <eduardodsxavier@gmail.com>
2024-11-08 15:54:32 -0300

    implement BFSGraphMatrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,45 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const seen = Array(graph.length).fill(false);
+    const prev = Array(graph.length).fill(-1);
+    const queue = [source];
+
+    seen[source] = true;
+
+    while (queue.length) {
+        const curr = queue.pop() as number;
+        if (curr === needle) {
+            break;
+        }
+
+        const ways = graph[curr]; 
+        for (let i = 0; i < ways.length; i++) {
+            if (ways[i] === 0) {
+                continue;
+            }
+            if (seen[i]) {
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = curr;
+            queue.unshift(i);
+        }
+    }
+
+    if (prev[needle] === -1) {
+        return null;
+    }
+
+    const out = [];
+    let curr = needle;
+
+    while (prev[curr] !== -1) {
+        out.unshift(curr);
+        curr = prev[curr];
+    }
+
+    out.unshift(curr);
+
+    return out;
 
 }
