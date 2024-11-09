9d212bfe6bb5bc326b2b51c0b15ee6a6af7392b1
eduardo <eduardodsxavier@gmail.com>
2024-11-09 11:32:32 -0300

    DFS on a graph list

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,37 @@
+function walk(graph: WeightedAdjacencyList, curr: number, needle: number, seen: boolean[], path: number[]): boolean {
+    if (seen[curr]) {
+        return false;
+    }
+
+    seen[curr] = true;
+    path.push(curr)
+
+    if (curr === needle) {
+        return true;
+    }
+
+    const ways = graph[curr];
+
+    for (let i = 0; i < ways.length; i++) {
+        if (walk(graph, ways[i].to, needle, seen, path)) {
+            return true;
+        }
+    }
+    path.pop();
+    return false;
+}
+
 export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+    const seen = Array(graph.length).fill(false);
+
+    const path = [] as number[];
+
+    walk(graph, source, needle, seen, path);
+
+    if (path.length <= 1) {
+        return null;
+    }
+
+    return path;
 
 }
